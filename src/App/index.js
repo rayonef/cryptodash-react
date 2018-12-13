d858655dae1c289809de7571cbfde446529f638b
Eduardo Rayón <rayon@MacBook-Pro-de-Eduardo.local>
2018-12-13 09:44:38 -0600

    add appProvider

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,16 @@ import './App.css';
 import Welcome from './WelcomeMessage';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import { AppProvider } from './AppProvider';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <AppBar />
-        <Welcome />
+        <AppProvider>
+          <AppBar />
+          <Welcome />
+        </AppProvider>
       </AppLayout>
     );
   }
