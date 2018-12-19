174f4d7c64b66cac9154ca9bdff374d5c42ab15f
Eduardo Rayón <rayon@MacBook-Pro-de-Eduardo.local>
2018-12-19 07:41:29 -0600

    finished dashboard grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -9,21 +9,24 @@ export const CoinGridStyled = styled.div`
   grid-gap: 15px;
   margin-top: 40px;
 `
+function getLowerSectionCoins(coinList, filteredCoins) {
+  return (filteredCoins && Object.keys(filteredCoins)) || Object.keys(coinList).slice(0, 100);
+}
 
-function getCoinsToDisplay(coinList, topSection, favorites) {
-  return topSection ? favorites : Object.keys(coinList).slice(0, 100);
+function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins) {
+  return topSection ? favorites : getLowerSectionCoins(coinList, filteredCoins);
 }
 
 export default function({topSection}) {
   return (
     <AppContext.Consumer>
-      {({coinList, favorites}) => 
+      {({coinList, favorites, filteredCoins}) => (
         <CoinGridStyled>
-          {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => 
-            <CoinTile topSection={topSection} coinKey={coinKey}/>
+          {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey => 
+            <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey}/>
           )}
         </CoinGridStyled>
-      }
+      )}
     </AppContext.Consumer>
   )
 }
